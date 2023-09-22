8fb77b3260ba87855ec561b62b14913c3ef9c438
Antonis Apostoloudis <74070585+antonisapostoloudis@users.noreply.github.com>
2023-09-22 18:34:49 +0300

    Update addition.m

--- tasks/matlab/easy/addition.m
+++ tasks/matlab/easy/addition.m
@@ -1,3 +1,11 @@
 % MATLAB - Easy
+% Input two numbers
+num1 = input('Enter the first number: ');
+num2 = input('Enter the second number: ');
 
+% Add the numbers
+result = num1 + num2;
+
+% Display the result
+fprintf('The sum of %f and %f is %f\n', num1, num2, result);
 % TODO: Create a MATLAB script that adds two numbers together and prints the result
